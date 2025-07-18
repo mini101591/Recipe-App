babae5a86f4faf4082b3c8b6868f2614ea062db8
mini <191342998+mini101591@users.noreply.github.com>
2025-07-18 23:04:33 +1000

    "Integrate Fake APIs from fetching recipes and posts..."

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import RecipeList from "@/components/RecipeList";
 import RecipeDetails from "@/components/RecipeDetails";
 import AboutScreen from "@/components/AboutScreen";
+import PostsScreen from "@/components/PostsScreen"
 
 const Drawer = createDrawerNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
         <Drawer.Screen name="Recipes" component={RecipeList}/>
         <Drawer.Screen name="Details" component={RecipeDetails}  initialParams={{ recipe: null }} />
         <Drawer.Screen name="About Us" component={AboutScreen}/>
+        <Drawer.Screen name="Blogs" component={PostsScreen}/>
     </Drawer.Navigator>
   );
 }
